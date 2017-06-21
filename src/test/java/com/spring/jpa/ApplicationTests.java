45d681ce55ea305fcf2221ab31f0f2b04efe61f2
win64 <win64@win64-PC>
2017-06-20 23:30:38 -0700

    demo jpa hibernate

---
+++ src/test/java/com/spring/jpa/ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.spring.jpa;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApplicationTests {
+	@Test
+	public void deftest() {
+		int salary = 96000; 
+		assertEquals(96000, salary, 0.0);
+	}
+}
