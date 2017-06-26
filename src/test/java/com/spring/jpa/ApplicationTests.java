77ed1a0a3478d66a15b49ee8f2217004dbfdf520
sanpraj32 <sanpraj32@sanpraj32-PC>
2017-06-26 23:28:17 +0530

    2 testcases added

--- src/test/java/com/spring/jpa/ApplicationTests.java
+++ src/test/java/com/spring/jpa/ApplicationTests.java
@@ -1,16 +1,23 @@
 package com.spring.jpa;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ApplicationTests {
 	@Test
-	public void deftest() {
+	public void testIndex() {
 		int salary = 96000; 
 		assertEquals(96000, salary, 0.0);
+	}
+	@Test
+	public void tesCreate() {
+		assertTrue(true);
 	}
 }
